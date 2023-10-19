790a1cebd23d5c3d4781d21ae81c104e25ff9b6e
shubh patel <pshubh420@gmail.com>
2023-10-19 11:32:04 +0530

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let item = document.querySelectorAll('.logo')
 
 let div = document.createElement('div')
+let tl = gsap.timeline()
 
 item.forEach((i)=>{
     //getting everyElement with class name .logo and passing everyElement to function animation
@@ -39,33 +40,54 @@ menubtn.addEventListener('click', ()=>{
     let largeMenu = document.querySelector('.all_menu')
     console.log(largeMenu)
     largeMenu.style.bottom = "3vw"
-    menubtn.style.top = '100%'
+    gsap.to(menubtn,{
+        top:'100%',
+        duration: 0.1
+    })
 })
 
 }
 menuDown()
 
 // text up animation
+function textAnimation(){
+
 gsap.from('.up h3',{
     y:'150%',
     delay: 0.2,
     duration: 0.6
    
 })
-gsap.from('.up h1',{
+tl.from('.up h1',{
     y:'150%',
     delay: 0.2,
     duration: 0.6
    
 })
 
+}
+textAnimation()
+
+// text down animation 
+function textDown(){
+    tl.from('.down p',{
+        y:'-150%',
+        duration:0.4,
+        stagger : 0.2
+    })
+    tl.from('.arrow',{
+        opacity : 0,
+        duration:0.3
+    })
+}
+textDown()
 // mousefollower animation functoin
 function mouseFollower(){
     let dot = document.querySelector('.circle')
     // console.log(dot)
     window.addEventListener('mousemove',(dets)=>{
-        let x  = dets.clientX-5
-        let y  = dets.clientY-5
+        let x  = dets.clientX-(dot.offsetWidth/2)
+        let y  = dets.clientY-(dot.offsetHeight/2)
         // console.log(x,y)
 
         gsap.to(dot,{
@@ -84,3 +106,23 @@ function mouseFollower(){
 }
 mouseFollower()
 
+//arrow animation 
+function arrow(){
+    let arrows = document.querySelectorAll('.arrow')
+    arrows.forEach((i)=>{
+        i.addEventListener('mouseenter',(e)=>{
+                let image = e.target.querySelector('img')
+                let tl = gsap.timeline()
+                console.log(image)
+                tl.to(image,{
+                    y:'100%',
+                    duration:0.4
+                })
+                tl.from(image,{
+                    y: '-100%',
+                    duration: 0.4
+                })
+            })
+    })
+}
+arrow()
