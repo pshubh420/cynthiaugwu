0f56dcc50bdf09e1b7a38b74967c6cb7498a29d5
shubh patel <pshubh420@gmail.com>
2023-10-20 11:34:17 +0530

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,105 +1,105 @@
 let item = document.querySelectorAll('.logo')
-
+let dot = document.querySelector('.circle')
 let div = document.createElement('div')
 let tl = gsap.timeline()
 
-item.forEach((i)=>{
+item.forEach((i) => {
     //getting everyElement with class name .logo and passing everyElement to function animation
-        // console.log(value)
-        animation(i)
-    
+    // console.log(value)
+    animation(i)
+
 })
 // bottom border animation
-function animation(i){
+function animation(i) {
     // running eventlistner on everyElement inside the nodelist "item"
-    i.addEventListener('mouseenter', ()=>{
+    i.addEventListener('mouseenter', () => {
         div.className = 'border'
         div.style.animationName = 'full'
         i.appendChild(div)
-        
-        
+
+
         console.log(i)
     })
-    
-    i.addEventListener('mouseleave', ()=>{
+
+    i.addEventListener('mouseleave', () => {
         div.style.animationName = 'zero'
-        setTimeout(()=>{
+        setTimeout(() => {
             i.removeChild(div)
-        },300)
+        }, 300)
     })
 }
 
 
 // on click menudown animation
-function menuDown(){
+function menuDown() {
 
     let menubtn = document.querySelector('.menubt')
 
-menubtn.addEventListener('click', ()=>{
-    
-    let largeMenu = document.querySelector('.all_menu')
-    console.log(largeMenu)
-    largeMenu.style.bottom = "3vw"
-    gsap.to(menubtn,{
-        top:'100%',
-        duration: 0.1
+    menubtn.addEventListener('click', () => {
+
+        let largeMenu = document.querySelector('.all_menu')
+        console.log(largeMenu)
+        largeMenu.style.bottom = "3vw"
+        gsap.to(menubtn, {
+            top: '100%',
+            duration: 0.1
+        })
     })
-})
 
 }
 menuDown()
 
 // text up animation
-function textAnimation(){
+function textAnimation() {
 
-gsap.from('.up h3',{
-    y:'150%',
-    delay: 0.2,
-    duration: 0.6
-   
-})
-tl.from('.up h1',{
-    y:'150%',
-    delay: 0.2,
-    duration: 0.6
-   
-})
+    gsap.from('.up h3', {
+        y: '150%',
+        delay: 0.2,
+        duration: 0.6
+
+    })
+    tl.from('.up h1', {
+        y: '150%',
+        delay: 0.2,
+        duration: 0.6
+
+    })
 
 }
 textAnimation()
 
 // text down animation 
-function textDown(){
-    tl.from('.down p',{
-        y:'-150%',
-        duration:0.4,
-        stagger : 0.2
+function textDown() {
+    tl.from('.down p', {
+        y: '-150%',
+        duration: 0.4,
+        stagger: 0.2
     })
-    tl.from('.arrow',{
-        opacity : 0,
-        duration:0.3
+    tl.from('.arrow', {
+        opacity: 0,
+        duration: 0.3
     })
 }
 textDown()
 // mousefollower animation functoin
-function mouseFollower(){
-    let dot = document.querySelector('.circle')
+function mouseFollower() {
+    
     // console.log(dot)
-    window.addEventListener('mousemove',(dets)=>{
-        let x  = dets.clientX-(dot.offsetWidth/2)
-        let y  = dets.clientY-(dot.offsetHeight/2)
+    window.addEventListener('mousemove', (dets) => {
+        let x = dets.clientX - (dot.offsetWidth / 2)
+        let y = dets.clientY - (dot.offsetHeight / 2)
         // console.log(x,y)
 
-        gsap.to(dot,{
-            scale:1,
-            x:x,
-            y:y
+        gsap.to(dot, {
+            scale: 1,
+            x: x,
+            y: y
         })
-        
+
     })
-    document.body.addEventListener('mouseleave',()=>{
-        gsap.to(dot,{
-            scale:0
+    document.body.addEventListener('mouseleave', () => {
+        gsap.to(dot, {
+            scale: 0
 
         })
     })
@@ -107,78 +107,99 @@ function mouseFollower(){
 mouseFollower()
 
 //arrow animation 
-function arrow(){
+function arrow() {
     let arrows = document.querySelectorAll('.arrow')
-    arrows.forEach((i)=>{
-        i.addEventListener('mouseenter',(e)=>{
-                let image = e.target.querySelector('img')
-                let tl = gsap.timeline()
-                console.log(image)
-                tl.to(image,{
-                    y:'100%',
-                    duration:0.4
-                })
-                tl.from(image,{
-                    y: '-100%',
-                    duration: 0.4
-                })
+    arrows.forEach((i) => {
+        i.addEventListener('mouseenter', (e) => {
+            let image = e.target.querySelector('img')
+            let tl = gsap.timeline()
+            console.log(image)
+            tl.to(image, {
+                y: '100%',
+                duration: 0.4
             })
+            tl.from(image, {
+                y: '-100%',
+                duration: 0.4
+            })
+        })
     })
 }
 arrow()
 
 //page2 text hover and animations
 
-function page2Animation(){
-    let divs= document.querySelectorAll('.page2_text')
+function page2Animation() {
+    let divs = document.querySelectorAll('.page2_text')
+    let big_circle = document.querySelector('.view')
     
-    divs.forEach((i)=>{
+
+    divs.forEach((i) => {
+
         let text = i.querySelector('h1')
         let image = i.querySelector('img')
-        let circle = document.querySelector('.circle')
-        i.addEventListener('mouseenter', (dets)=>{
-            gsap.to(text,{
-                x:40,
+
+
+
+        i.addEventListener('mouseenter', (dets) => {
+
+
+
+            gsap.to(text, {
+                x: 40,
                 opacity: 0.4
             })
-            
+
+
 
         })
-        i.addEventListener('mouseleave',()=>{
-            gsap.to(text,{
-                x:0,
-                opacity:1
+
+        i.addEventListener('mouseleave', () => {
+            gsap.to(text, {
+                x: 0,
+                opacity: 1
             })
 
-            gsap.to(image,{
+            gsap.to(image, {
+                opacity: 0
+            })
+
+            dot.style.display = 'block'
+            gsap.to(big_circle,{
+                scale: 0,
                 opacity:0
             })
 
-            gsap.to(circle,{
-                clearPropes: 'opacity '
 
-            })
         })
 
 
-        i.addEventListener('mousemove',(dets)=>{
+        i.addEventListener('mousemove', (dets) => {
+            
+
             let diff = i.getBoundingClientRect()
-            let x = (dets.clientX - diff.left)-(image.offsetWidth/2)
+            console.log(big_circle.offsetHeight/2,big_circle.offsetWidth/2)
+            
             let y = (dets.clientY - diff.top) - (image.offsetHeight/2)
-            
+            let x = (dets.clientX - diff.left) - (image.offsetWidth/2)
 
-            gsap.to(circle,{
-                opacity:0.8,
-                scale:10
-            })
-            
-            gsap.to(image,{
+            gsap.to(image, {
                 y: y,
-                x:x,
-                opacity:1
+                x: x,
+                opacity: 1
             })
 
-            gsap.to()
+            dot.style.display = 'none'
+
+            gsap.to(big_circle,{
+                scale:1,
+                opacity:1,
+                x: dets.clientX - (big_circle.offsetWidth/2),
+                y: dets.clientY - (big_circle.offsetHeight/2)
+            })
+
+
+
         })
     })
 }
